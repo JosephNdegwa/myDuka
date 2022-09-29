e1f76f4a81f36aee8c34ccd3c08e9e9ca48646e8
josephndegwa <kariukindegwa98@gmail.com>
2022-09-29 16:56:09 +0300

    add serializers for clothes model

--- Server/duka/models.py
+++ Server/duka/models.py
@@ -1,3 +1,14 @@
+from distutils.command.upload import upload
+from email.mime import image
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+class Clothes(models.Model):
+    name = models.CharField(max_length=40)
+    image = models.ImageField(upload_to='images/')
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
---
+++ Server/duka/serializer.py
@@ -0,0 +1,9 @@
+from dataclasses import field
+from rest_framework import serializers
+from .models import Clothes
+
+
+class ClotheSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Clothes
+        fields = ('name','image','price','description')
